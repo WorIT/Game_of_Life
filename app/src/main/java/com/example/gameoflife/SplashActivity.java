ee0c01651f658cd833244e7d99fb292c0da4e77a
Igor Nechaev (WorIT) <Nechaev.World@gmail.com>
2020-06-10 13:30:34 +0300

    init

---
+++ app/src/main/java/com/example/gameoflife/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.gameoflife;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+
+        new Thread() {
+            public void run() {
+                // загрузка начальных данных пользователя
+
+
+                // имитация загрузки
+                imitateLoading();
+
+                // после загрузки переходим в главное меню
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);// по-умолчанию после загрузки запускается список операций
+                startActivity(intent);
+            }
+        }.start();
+
+
+    }
+
+    private void imitateLoading() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
