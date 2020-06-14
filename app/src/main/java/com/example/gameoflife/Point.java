55f7eca3ce3daab9717c27ed5febdd7a8244400a
Igor Nechaev (WorIT) <Nechaev.World@gmail.com>
2020-06-14 20:44:30 +0300

    2

---
+++ app/src/main/java/com/example/gameoflife/Point.java
@@ -0,0 +1,41 @@
+package com.example.gameoflife;
+
+
+public class Point {
+    private int x;
+    private int y;
+    public boolean flag;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x &&
+                y == point.y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
